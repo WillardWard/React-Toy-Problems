a469bf91afe7de8256962090fd3d29d959757cf0
Willard Ward <willardwardiii@gmail.com>
2023-03-30 17:45:29 -0500

    Working on Evens and Odds

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
 class EvenAndOdd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: [],
+    };
+  }
+
   render() {
-    return <p>EvenAndOdd</p>;
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens and Odds</h4>
+        <input
+          className="inputLine"
+          onChange={(event) => this.setState({ userInput: event.target.value })}
+        ></input>
+        <button className="confirmationButton"></button>
+        <span className="resultsBox"></span>
+        <span className="resultsBox"></span>
+      </div>
+    );
   }
 }
 
