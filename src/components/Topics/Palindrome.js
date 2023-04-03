668203a8135f6c2ed6b3e90242eadf2de932de0f
Willard Ward <willardwardiii@gmail.com>
2023-04-03 17:47:10 -0500

    Working on last toy problem

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
 
 class Palindrome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: "",
+      palindrome: "",
+    };
+  }
+
+  isPalendrome = (prop) => {
+    let userString = prop.toLowerCase().replace(/\s+/g, "");
+    let halfLength = userString.length / 2;
+    let checkPalindrome = true;
+
+    if (halfLength % 2 !== 0) {
+      halfLength = Math.floor(halfLength);
+    }
+
+    for (let i = 0; i < halfLength; i++) {
+      if (
+        userString.charAt(i) !== userString.charAt(userString.length - 1 - i)
+      ) {
+        checkPalindrome = false;
+      }
+    }
+
+    checkPalindrome
+      ? this.setState({ palindrome: "You have a palindrome!" })
+      : this.setState({ palindrome: "This is not a palindrome!" });
+  };
+
   render() {
-    return <p>Palindrome</p>;
+    return (
+      <div className="puzzleBox filterStringPB">
+        <h4>Palindrome</h4>
+        <input
+          className="inputLine"
+          onChange={(e) => this.setState({ userInput: e.target.value })}
+        ></input>
+        <button
+          className="confirmationButton"
+          onClick={() => this.isPalendrome(this.state.userInput)}
+        >
+          Check Palindrome
+        </button>
+        <span className="resultsBox">Results: {this.state.palindrome}</span>
+      </div>
+    );
   }
 }
 
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 class Sum extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number1: 0,
+      number2: 0,
+      sum: null,
+    };
+  }
+
   render() {
-    return <p>Sum</p>;
+    return (
+      <div className="puzzleBox sumPB">
+        <h4>Sum</h4>
+        <input
+          className="inputLine"
+          onChange={(e) => this.setState({ number1: e.target.value })}
+        ></input>
+        <input
+          className="inputLine"
+          onChange={(e) => this.setState({ number2: e.target.value })}
+        ></input>
+        <button
+          className="confirmationButton"
+          onClick={() => this.sumValues(this.state.number1, this.state.number2)}
+        >
+          Calculate Sum
+        </button>
+        <span className="resultsBox">Sum: {this.state.sum}</span>
+      </div>
+    );
   }
 }
 
